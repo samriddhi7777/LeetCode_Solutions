03c44ad822809cd1acbbf84f2eadb9c327010ecd
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-07-03 16:50:17 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/Top K Frequent Elements/Solution.cpp
+++ LeetCode/C++/Medium/Top K Frequent Elements/Solution.cpp
@@ -1,7 +1,22 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
+        unordered_map<int, int> freq;
+
+        for(int num : nums){
+            freq[num]++;
+        }
+        priority_queue<pair<int, int>> pq;
+        for(auto it : freq){
+            pq.push({it.second, it.first});
+        }
+        vector<int> ans;
+
+        while(k--){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
         
     }
 };
