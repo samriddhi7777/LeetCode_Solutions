be49e1d15598efa2be6b3a212f27ee8cf49667b4
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-07-05 16:23:28 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/Valid Palindrome/Solution.cpp
+++ LeetCode/C++/Easy/Valid Palindrome/Solution.cpp
@@ -1,25 +1,40 @@
-class Solution {
-public:
-    bool isPalindrome(string s) {
+class Solution{
+    public:
+    bool isPalindrome(string s){
         int left = 0;
         int right = s.length() - 1;
-
-        while(left < right) {
-            if(!isalnum(s[left])) {
-                left++;
-            }
-            else if(!isalnum(s[right])){
-                right--;
-            }
-            else {
-                if(tolower(s[left]) != tolower(s[right])){
+        while(left < right){
+            if(!(s[left] >= 'a' && s[left] <= 'z' ||
+                 s[left] >= 'A' && s[left] <= 'Z' ||
+                 s[left] >= '0' && s[left] <= '9')){
+                     left++;
+                 }
+            else if(!(s[right] >= 'a' && s[right] <= 'z' ||
+                 s[right] >= 'A' && s[right] <= 'Z' ||
+                 s[right] >= '0' && s[right] <= '9')){
+                     right--;
+                 }
+            else{
+               char leftChar = s[left];
+               char rightChar = s[right];
+               
+               if(s[left] >= 'A' && s[left] <= 'Z'){
+                   leftChar = leftChar + 32;
+        
+                 
+                }
+                 if(s[right] >= 'A' && s[right] <= 'Z'){
+                   rightChar = rightChar + 32;
+        
+                 
+                }
+                if(leftChar != rightChar){
                     return false;
                 }
                 left++;
                 right--;
             }
+            }
+            return true;
         }
-        return true;
-        
     }
-};
