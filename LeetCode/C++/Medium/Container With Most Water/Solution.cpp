4d041a63c24e443cd04382847ec8f067d6536304
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-07-09 19:04:37 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/Container With Most Water/Solution.cpp
+++ LeetCode/C++/Medium/Container With Most Water/Solution.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int 
+        int maxWater = 0;
+        int lp = 0; int rp = height.size()-1;
+        while(lp < rp){
+            int w = rp - lp;
+            int ht = min(height[lp], height[rp]);
+            int currWater = w * ht;
+            maxWater = max(maxWater, currWater);
+            height[lp] < height[rp] ? lp++ : rp--;
+        }
+        return maxWater;
         
     }
 };
