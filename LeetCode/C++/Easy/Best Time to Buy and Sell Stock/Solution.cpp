91ca66a8ee35eb472a0714b713e9249ec67c9e5f
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-07-10 17:51:26 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/Best Time to Buy and Sell Stock/Solution.cpp
+++ LeetCode/C++/Easy/Best Time to Buy and Sell Stock/Solution.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minprice = 
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int i = 1;i < prices.size(); i++){
+            int todayProfit = prices[i] - minPrice;
+            maxProfit = max(maxProfit, todayProfit);
+            minPrice = min(minPrice, prices[i]);
+        }
+        return maxProfit;
         
     }
 };
