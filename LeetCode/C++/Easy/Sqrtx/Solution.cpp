badda21b3a64d2ea583185a1167b6e28b6e1f24b
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-07-17 20:44:37 +0530

    Add Easy C++ Solution from LeetCode

--- LeetCode/C++/Easy/Sqrtx/Solution.cpp
+++ LeetCode/C++/Easy/Sqrtx/Solution.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        
+        if(x == 0)
+        return 0;
+
+        int left = 1;
+        int right = x;
+        int ans = 0;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            long long square = 1LL * mid * mid;
+            if(square == x)
+            return mid;
+
+            else if(square < x){
+                ans = mid;
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+        return ans;
         
     }
 };
