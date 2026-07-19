783b75ecf1a4d40c35f267e19cfbfbb43b87af1d
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-07-19 19:53:00 +0530

    Add Medium C++ Solution from LeetCode

--- LeetCode/C++/Medium/981. Time Based Key-Value Store/Solution.cpp
+++ LeetCode/C++/Medium/981. Time Based Key-Value Store/Solution.cpp
@@ -1,42 +1,43 @@
 class TimeMap {
 public:
-    unordered_map<string, vector<pair<int,string>>>mp;
+    unordered_map<string, vector<pair<int, string>>> mp;
+
     TimeMap() {
-        
+
     }
 
-    
     void set(string key, string value, int timestamp) {
-        mp[key].push_back({timestamp,value});
-        
+        mp[key].push_back({timestamp, value});
     }
-    
+
     string get(string key, int timestamp) {
-        if(mp.find(key)== mp.end())
-           return " ";
-        vector<pair<int,string>>& arr = mp[key];
+
+        // If the key does not exist
+        if (mp.find(key) == mp.end()) {
+            return "";
+        }
+
+        vector<pair<int, string>> &arr = mp[key];
 
         int left = 0;
         int right = arr.size() - 1;
 
         string ans = "";
 
-       while(left <= right){
+        while (left <= right) {
 
-    int mid = left + (right-left)/2;
+            int mid = left + (right - left) / 2;
 
-    if(arr[mid].first <= timestamp){
+            // Current timestamp is valid
+            if (arr[mid].first <= timestamp) {
+                ans = arr[mid].second;      // Store current best answer
+                left = mid + 1;             // Search for a later valid timestamp
+            }
+            else {
+                right = mid - 1;            // Search in left half
+            }
+        }
 
-        ans = arr[mid].second;
-        left = mid + 1;
-    }
-    else{
-
-        right = mid - 1;
-    }
-}
         return ans;
-        
     }
 };
-
