7e2b3e8f3835ebe1cb52bf28bf7faf983532d3dd
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-07-23 21:44:58 +0530

    Add Medium C++ Solution from LeetCode

---
+++ LeetCode/C++/Medium/146. LRU Cache/Solution.cpp
@@ -0,0 +1,82 @@
+class Node {
+public:
+    int key, value;
+    Node* prev;
+    Node* next;
+
+    Node(int k, int v) {
+        key = k;
+        value = v;
+        prev = next = nullptr;
+    }
+};
+
+class LRUCache {
+public:
+    int capacity;
+    unordered_map<int, Node*> mp;
+
+    Node* head;
+    Node* tail;
+
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    void remove(Node* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void insert(Node* node) {
+        node->next = head->next;
+        node->prev = head;
+
+        head->next->prev = node;
+        head->next = node;
+    }
+
+    int get(int key) {
+        if (mp.find(key) == mp.end())
+            return -1;
+
+        Node* node = mp[key];
+
+        remove(node);
+        insert(node);
+
+        return node->value;
+    }
+
+    void put(int key, int value) {
+        if (mp.find(key) != mp.end()) {
+            Node* node = mp[key];
+            node->value = value;
+
+            remove(node);
+            insert(node);
+
+            return;
+        }
+
+        if (mp.size() == capacity) {
+            Node* lru = tail->prev;
+
+            remove(lru);
+            mp.erase(lru->key);
+
+            delete lru;
+        }
+
+        Node* node = new Node(key, value);
+
+        insert(node);
+        mp[key] = node;
+    }
+};
