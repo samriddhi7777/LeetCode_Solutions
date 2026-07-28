739dfccbc116fc16196b0422dc74cddbda125df5
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-07-28 12:56:49 +0530

    Add Medium C++ Solution from LeetCode

---
+++ LeetCode/C++/Medium/450. Delete Node in a BST/Solution.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    TreeNode* findMin(TreeNode* root) {
+        while (root->left != NULL) {
+            root = root->left;
+        }
+        return root;
+    }
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+
+        if (root == NULL)
+            return NULL;
+
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        }
+        else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        }
+        else {
+
+            // No left child
+            if (root->left == NULL) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }
+
+            // No right child
+            if (root->right == NULL) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+
+            // Two children
+            TreeNode* temp = findMin(root->right);
+
+            root->val = temp->val;
+
+            root->right = deleteNode(root->right, temp->val);
+        }
+
+        return root;
+    }
+};
