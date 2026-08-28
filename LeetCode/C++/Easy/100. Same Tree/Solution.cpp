512bd4ade66f5ebd5f3af93a40434f63493f4dcb
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-08-28 11:18:19 +0530

    Add Easy C++ Solution for 100. Same Tree from LeetCode

--- LeetCode/C++/Easy/100. Same Tree/Solution.cpp
+++ LeetCode/C++/Easy/100. Same Tree/Solution.cpp
@@ -1,15 +1,16 @@
-class Solution{
+
+class Solution {
 public:
-     bool isSameTree(TreeNode* p, TreeNode* q){
-        if(p == NULL && q == NULL)
-           return true;
-        if(p == NULL || q == NULL)
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr)
+          return true;
+        if(p == nullptr || q == nullptr)
            return false;
         if(p->val != q->val)
-           return false;
+          return false;
+        
         return isSameTree(p->left , q->left) &&
-               isSameTree(p->right , q->right);
-     }
-     
-
+               isSameTree(p->right, q->right);
+        
+    }
 };
