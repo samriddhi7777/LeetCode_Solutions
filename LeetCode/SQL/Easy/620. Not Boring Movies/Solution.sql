dc9319a0fbc3c5f97b5f16011d27765cf6dc4a4d
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-08-29 10:39:29 +0530

    Add Easy SQL Solution for 620. Not Boring Movies from LeetCode

--- LeetCode/SQL/Easy/620. Not Boring Movies/Solution.sql
+++ LeetCode/SQL/Easy/620. Not Boring Movies/Solution.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-SELECT *
+SELECT id,movie, description, rating
 FROM Cinema
-WHERE id%2 <> 0
-AND description <> "boring"
-ORDER BY rating DESC
+WHERE id%2 != 0 AND description != 'boring'
+ORDER BY rating desc
