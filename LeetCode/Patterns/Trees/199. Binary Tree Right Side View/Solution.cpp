a7fd0fcdb2eeafc4a590b30a64d08f3d6ea37f78
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-08-30 08:47:32 +0530

    Add Medium C++ Solution for 199. Binary Tree Right Side View from LeetCode

--- LeetCode/Patterns/Trees/199. Binary Tree Right Side View/Solution.cpp
+++ LeetCode/Patterns/Trees/199. Binary Tree Right Side View/Solution.cpp
@@ -1,40 +1,35 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        if(root == NULL)
-           return {};
         vector<int> result;
+
+        if(root == nullptr)
+           return result;
+        
         queue<TreeNode*> q;
         q.push(root);
 
         while(!q.empty()){
-            int n = q.size();
-            TreeNode* node = NULL;
+           int size = q.size();
 
-            while(n--){
-                node = q.front();
+            
+            for(int i = 0; i < size; i++){
+                TreeNode* node = q.front();
                 q.pop();
 
-                if(node->left != NULL)
-                   q.push(node->left);
-                if(node->right != NULL)
-                   q.push(node->right); 
+                if(i == size - 1)
+                  result.push_back(node->val);
+
+                if(node->left != nullptr)
+                  q.push(node->left);
+
+                if(node->right != nullptr)
+                   q.push(node->right);
+                
+
             }
-            result.push_back(node->val);
         }
         return result;
-
         
     }
 };
