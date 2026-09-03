a5f4532d67777a39a7b07ac49851ec34ea5bcc7d
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-09-03 12:26:22 +0530

    Add Easy SQL Solution for 619. Biggest Single Number from LeetCode

--- LeetCode/Patterns/Database/619. Biggest Single Number/Solution.sql
+++ LeetCode/Patterns/Database/619. Biggest Single Number/Solution.sql
@@ -1,10 +1,9 @@
 # Write your MySQL query statement below
-WITH cte AS
-(SELECT num
+SELECT MAX(num) as num
 FROM MyNumbers
-GROUP BY num
-HAVING COUNT(num) = 1)
-
-SELECT CASE WHEN COUNT(*) > 0 THEN MAX(num)
-ELSE NULL END AS num
-FROM cte
+WHERE num IN(
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*) = 1
+)
