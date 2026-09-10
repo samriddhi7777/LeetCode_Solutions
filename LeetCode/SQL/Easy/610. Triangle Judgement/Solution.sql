7f208cb72ed532c14287ba1766bd9c74ab757159
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-09-10 09:15:23 +0530

    Add Easy SQL Solution for 610. Triangle Judgement from LeetCode

--- LeetCode/SQL/Easy/610. Triangle Judgement/Solution.sql
+++ LeetCode/SQL/Easy/610. Triangle Judgement/Solution.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT *, CASE WHEN x+y>z AND y+z>x AND x+z >y THEN 'Yes'
-ELSE 'No' END AS triangle
-FROM Triangle  
+SELECT *,
+   IF(x+y>z AND y+z>x AND x+z>y , "Yes", "No") AS triangle
+FROM Triangle
