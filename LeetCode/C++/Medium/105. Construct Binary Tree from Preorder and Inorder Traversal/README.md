e022ffd297a97d3ba8dfff3562f7e6e2bc73e36a
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-09-25 01:23:18 +0530

    Add Medium C++ Solution for 105. Construct Binary Tree from Preorder and Inorder Traversal from LeetCode

--- LeetCode/C++/Medium/105. Construct Binary Tree from Preorder and Inorder Traversal/README.md
+++ LeetCode/C++/Medium/105. Construct Binary Tree from Preorder and Inorder Traversal/README.md
@@ -8,8 +8,8 @@
 Array, Hash Table, Divide and Conquer, Tree, Binary Tree
 
 ### 🚀 Performance
-- **Runtime:** 4 ms
-- **Memory:** 27.3 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/C++/Medium/105. Construct Binary Tree from Preorder and Inorder Traversal/Solution.cpp
+++ LeetCode/C++/Medium/105. Construct Binary Tree from Preorder and Inorder Traversal/Solution.cpp
@@ -1,31 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    unordered_map<int, int> inorderIndex;
-    int preorderIndex = 0;
-
-    TreeNode* build(vector<int>& preorder, int left, int right){
-        if(left > right){
-
-        
-          return nullptr;
-        }
-        int rootValue = preorder[preorderIndex++];
-        TreeNode* root = new TreeNode(rootValue);
-
-        int mid = inorderIndex[rootValue];
-
-        root->left = build(preorder, left, mid - 1);
-        root->right = build(preorder, mid + 1, right);
-
-        return root;
-
-    }
+    unordered_map<int,int> inMap;
+    int preIndex = 0;
     
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        for(int i = 0; i < inorder.size(); i++){
-            inorderIndex [inorder[i]] = i;
-        }
-        return build(preorder, 0, inorder.size() - 1);
         
-    }
+        TreeNode* build(vector<int> &preorder, int inStart,int inEnd){
+            if(inStart > inEnd)
+               return nullptr;
+
+            int rootValue = preorder[preIndex++];
+            TreeNode* root = new TreeNode(rootValue);
+
+            int rootIndex = inMap[rootValue];
+
+            root->left = build(preorder, inStart, rootIndex - 1 );
+            root->right = build(preorder, rootIndex + 1, inEnd);
+
+            return root;
+        }
+        TreeNode* buildTree(vector<int> &preorder, vector<int> &inorder){
+            for(int i = 0; i < inorder.size(); i++){
+                inMap[inorder[i]] = i;
+            }
+            return build(preorder,0,inorder.size() - 1);
+
+        }
+        
+    
 };
