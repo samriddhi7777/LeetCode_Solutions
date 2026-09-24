7d140a316d4137e6d56ec6f8026f35b7fa61cd27
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-09-25 03:44:34 +0530

    Add Hard C++ Solution for 124. Binary Tree Maximum Path Sum from LeetCode

--- LeetCode/C++/Hard/124. Binary Tree Maximum Path Sum/README.md
+++ LeetCode/C++/Hard/124. Binary Tree Maximum Path Sum/README.md
@@ -8,8 +8,8 @@
 Dynamic Programming, Tree, Depth-First Search, Binary Tree, DP on Trees
 
 ### 🚀 Performance
-- **Runtime:** 0 ms
-- **Memory:** 27.9 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Patterns/Dynamic Programming/124. Binary Tree Maximum Path Sum/Solution.cpp
+++ LeetCode/Patterns/Dynamic Programming/124. Binary Tree Maximum Path Sum/Solution.cpp
@@ -1,22 +1,32 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int ans = INT_MIN;
-    int solve(TreeNode* root){
-        if(root == nullptr){
-            return 0;
-        }
-        int leftGain = max(0, solve(root->left));
-        int rightGain = max(0, solve(root->right));
+    int maxSum = INT_MIN;
+    int dfs(TreeNode* root){
+        if(root == nullptr)
+           return 0;
+        int leftgain = max(0,dfs(root->left));
+        int rightgain = max(0,dfs(root->right));
 
-        int currentPath = root->val + leftGain + rightGain;
+        int currentPath = leftgain + root->val + rightgain;
+        maxSum = max(maxSum, currentPath);
 
-        ans = max(currentPath, ans);
-        return root->val + max(leftGain, rightGain);
+        return root->val + max(leftgain,rightgain);
+
     }
     int maxPathSum(TreeNode* root) {
-        solve(root);
-        return ans;
+        dfs(root);
+        return maxSum;
         
     }
 };
