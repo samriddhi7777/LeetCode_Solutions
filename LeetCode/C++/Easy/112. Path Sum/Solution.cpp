7bf5208783fcaf823a259f5be687dce9888c6795
samriddhi7777 <samriddhithakur4321@gmail.com>
2026-09-25 02:13:19 +0530

    Add Easy C++ Solution for 112. Path Sum from LeetCode

--- LeetCode/C++/Easy/112. Path Sum/Solution.cpp
+++ LeetCode/C++/Easy/112. Path Sum/Solution.cpp
@@ -1,26 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool fun(TreeNode* root, int sum, int target) {
-        
-        if (root == NULL)
-            return false;
-
-        sum += root->val;
-
-        // If current node is a leaf
-        if (root->left == NULL && root->right == NULL) {
-            if (sum == target)
-                return true;
-
-            return false;
-        }
-
-        // Check left and right subtree
-        return fun(root->left, sum, target) ||
-               fun(root->right, sum, target);
-    }
-
     bool hasPathSum(TreeNode* root, int targetSum) {
-        return fun(root, 0, targetSum);
+        if(root == nullptr)
+           return false;
+        
+        if(root->left == nullptr && root->right == nullptr){
+            return targetSum == root->val;
+        }
+        targetSum -= root->val;
+
+        return hasPathSum(root->left, targetSum) ||
+               hasPathSum(root->right, targetSum);
+           
+        
     }
 };
